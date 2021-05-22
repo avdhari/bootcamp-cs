8c1f3b739c6edbc6e02582f5e9370eb9952afc37
avdhari <avdhari114@gmail.com>
2021-05-22 12:48:10 +0530

    added post detail new post,(TemplateDoesNotExist issue)

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView, name='home'),
+    path('newpost/', views.NewPost, name='newpost'),
+    path('post/<int:pk>', views.PostDetail, name='postDetail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
+
 
 
 
 def HomeView(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-date_created')
     context = {
         'posts': posts
     }
     
     return render(request, 'blog/home.html', context)
+
+
+
+
+
+def NewPost(request):
+        
+    form = PostForm()
+    if request.method == 'POST':
+    #print('Printing POST:', request.POST)    
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+
+    context = {'form':form}
+
+    return render(request, 'blog/newpost.html', context)
+
+
+def PostDetail(request, pk):
+    post = Post.objects.get(id=pk)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'blog/postDetail.html', context)
